22da5046611ad226a54ea9576520c6b981699db3
Soi-coding <2023112594@dgu.ac.kr>
2025-11-05 14:26:34 +0900

    [Feat] 헤더 바 구현 완료

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import H from "./Header.styles.ts";
+import BK from "../assets/BK.png";
+
+export default function Header() {
+  return (
+    <H.Container>
+      <H.Inner>
+        <H.Logo src={BK} alt="BK Logo" />
+
+        <H.NavMenu>
+          <H.NavItem>패키지</H.NavItem>
+          <H.NavItem>생성</H.NavItem>
+          <H.NavItem>패스</H.NavItem>
+          <H.NavItem>마이페이지</H.NavItem>
+        </H.NavMenu>
+
+        <H.UserMenu>
+          <H.SignUp>회원가입</H.SignUp>
+          <H.LoginButton>로그인</H.LoginButton>
+        </H.UserMenu>
+      </H.Inner>
+    </H.Container>
+  );
+}
